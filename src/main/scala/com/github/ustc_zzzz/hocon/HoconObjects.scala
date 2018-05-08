b3a168f4e5a7ab3b0f40149a6762d2092771ce39
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2018-05-08 16:56:32 +0800

    Make it possible to capture indexes in the parser

--- src/main/scala/com/github/ustc_zzzz/hocon/HoconObjects.scala
+++ src/main/scala/com/github/ustc_zzzz/hocon/HoconObjects.scala
@@ -1,7 +1,5 @@
 package com.github.ustc_zzzz.hocon
 
-import com.github.ustc_zzzz.hocon.HoconObjects.{Element, ElementCollection}
-
 /**
   * @author ustc_zzzz
   */
@@ -23,40 +21,82 @@ object HoconObjects {
 
   case class MultilineString(value: String) extends Element
 
-  case class FieldPathPart(value: String) extends Element
+  case class PathExpressionPart(value: String) extends Element
 
-  case class FieldPath(values: Traversable[Element]) extends ElementCollection(values, "FieldPath") {
-    override lazy val value: String = values.map(_.value).mkString(".")
+  case class PathExpression(path: Seq[Element]) extends ElementCollection("PathExpression") {
+    override lazy val value: String = path.map(_.value).mkString(".")
+
+    override val values: IndexedSeq[Element] = path.toIndexedSeq
+
+    override def offset(i: Int): Int = if (i > 0) 1 else 0
   }
 
-  case class Concatenation(values: Traversable[Element]) extends ElementCollection(values, "Concatenation")
+  case class SubstitutionWeak(path: PathExpression) extends ElementCollection("SubstitutionWeak") {
+    override def offset(index: Int): Int = if (index > 0) 1 else 3
 
-  case class SubstitutionWeak(values: FieldPath) extends ElementCollection(values.values, "SubstitutionWeak") {
-    override lazy val value: String = "${?" + values.value + "}"
+    override lazy val value: String = "${?" + path.value + "}"
+
+    override val values: IndexedSeq[Element] = path.values
   }
 
-  case class SubstitutionStrong(values: FieldPath) extends ElementCollection(values.values, "SubstitutionStrong") {
-    override lazy val value: String = "${" + values.value + "}"
+  case class SubstitutionStrong(path: PathExpression) extends ElementCollection("SubstitutionStrong") {
+    override def offset(index: Int): Int = if (index > 0) 1 else 2
+
+    override lazy val value: String = "${" + path.value + "}"
+
+    override val values: IndexedSeq[Element] = path.values
   }
 
-  case class List(values: Traversable[Element]) extends ElementCollection(values, "List") {
+  case class List(start: Int, elements: IndexedSeq[ListElement], end: Int) extends ElementCollection("List") {
     override lazy val value: String = values.map(_.value).mkString("[", ", ", "]")
+
+    override val values: IndexedSeq[Element] = elements.map(_.element)
+
+    override def offset(index: Int): Int = elements(index).offset
+
+    override def size: Int = end - start
   }
 
-  case class Object(values: Traversable[(FieldPath, Element)]) extends ElementCollection(values.map(_._2), "") {
-    override lazy val value: String = values.map(t => s"${t._1.value}: ${t._2.value}").mkString("{", ", ", "}")
+  case class Object(start: Int, elements: IndexedSeq[ObjectElement], end: Int) extends ElementCollection("Object") {
+    override def toString: String = elements.map(t => s"${t.path} -> ${t.element._2}").mkString("Object(", ", ", ")")
 
-    override def toString: String = values.map(t => s"${t._1} -> ${t._2}").mkString("Object(", ", ", ")")
+    override lazy val value: String = elements.map(t => s"${t.path}: ${t.element._2}").mkString("{", ", ", "}")
+
+    override def offset(i: Int): Int = if ((i & 1) > 0) elements(i / 2).element._1 else elements(i / 2).offset
+
+    override val values: IndexedSeq[Element] = elements.flatMap(e => IndexedSeq(e.path, e.element._2))
+
+    override def size: Int = end - start
+  }
+
+  case class Concatenation(start: Int, head: Element, last: Seq[(Int, Element)], end: Int) extends ElementCollection("Concatenation") {
+    override def offset(index: Int): Int = if (index > 0) last(index - 1)._1 else start // TODO: Performance
+
+    override val values: IndexedSeq[Element] = head +: last.map(_._2).toIndexedSeq
+
+    def children: Seq[(Int, Element)] = (start, head) +: last
+
+    override def size: Int = end - start
   }
 
   sealed trait Element {
+    def size: Int = value.length
+
     def value: String
   }
 
-  sealed abstract class ElementCollection(values: Traversable[Element], name: String) extends Element {
+  sealed abstract class ElementCollection(name: String) extends Element {
+    def offset(index: Int): Int
+
+    def values: IndexedSeq[Element]
+
     override lazy val value: String = values.map(_.value).mkString
 
     override def toString: String = values.mkString(name + "(", ", ", ")")
   }
+
+  case class ObjectElement(offset: Int, path: PathExpression, element: (Int, Element))
+
+  case class ListElement(offset: Int, element: Element)
 
 }
--- src/main/scala/com/github/ustc_zzzz/hocon/HoconParsers.scala
+++ src/main/scala/com/github/ustc_zzzz/hocon/HoconParsers.scala
@@ -10,69 +10,163 @@ object HoconParsers {
 
   import com.github.ustc_zzzz.hocon.HoconObjects._
 
-  private val digits: Parser[_] = P(CharsWhileIn(strings = '0' to '9'))
-  private val numeric: Parser[_] = P("0" | (CharIn(strings = '1' to '9') ~ digits.?))
-  private val exponent: Parser[_] = P(CharIn(strings = "eE") ~ CharIn(strings = "+-").? ~ digits)
-  private val unicode: Parser[_] = P("u" ~ CharIn(strings = '0' to '9', 'a' to 'f', 'A' to 'F').rep(exactly = 4))
+  private val digits: Parser[_] = P {
+    CharsWhileIn(strings = '0' to '9')
+  }
+  private val numeric: Parser[_] = P {
+    "0" | (CharIn(strings = '1' to '9') ~ digits.?)
+  }
+  private val exponent: Parser[_] = P {
+    CharIn(strings = "eE") ~ CharIn(strings = "+-").? ~ digits
+  }
+  private val unicode: Parser[_] = P {
+    "u" ~ CharIn(strings = '0' to '9', 'a' to 'f', 'A' to 'F').rep(exactly = 4)
+  }
 
-  private val timeUnitSuffixAbbr: Parser[_] = P("ns" | "us" | "ms" | "s" | "m" | "h" | "d")
-  private val timeUnitSuffix: Parser[_] = P(("nanosecond" | "microsecond" | "millisecond" | "second" | "minute" | "hour" | "day") ~ "s".?)
+  private val timeUnitSuffixAbbr: Parser[_] = P {
+    "ns" | "us" | "ms" | "s" | "m" | "h" | "d"
+  }
+  private val timeUnitSuffix: Parser[_] = P {
+    ("nanosecond" | "microsecond" | "millisecond" | "second" | "minute" | "hour" | "day") ~ "s".?
+  }
 
-  private val spaces: Parser[_] = P(CharsWhile(c => c.isWhitespace && c != '\n'))
-  private val comment: Parser[_] = P(("//" | "#") ~ CharsWhile(_ != '\n', min = 0))
-  private val spacesCrossLines: Parser[_] = P((CharsWhile(c => c.isWhitespace) | comment).rep())
+  private val spaces: Parser[_] = P {
+    CharsWhile(c => c.isWhitespace && c != '\n')
+  }
+  private val comment: Parser[_] = P {
+    ("//" | "#") ~ CharsWhile(_ != '\n', min = 0)
+  }
+  private val spacesCrossLines: Parser[_] = P {
+    (CharsWhile(c => c.isWhitespace) | comment).rep()
+  }
 
-  private val valueSeparator: Parser[_] = P((comment.? ~ "\n" ~ spacesCrossLines.? ~ ",".? ~ spacesCrossLines) | ("," ~/ spacesCrossLines))
+  private val valueSeparator: Parser[_] = P {
+    (comment.? ~ "\n" ~ spacesCrossLines.? ~ ",".? ~ spacesCrossLines) | ("," ~/ spacesCrossLines)
+  }
 
-  private val escape: Parser[_] = P("\\" ~ (CharIn(strings = "\"\\\\/bfnrt") | unicode))
-  private val quotedChar: Parser[_] = P(CharsWhile(c => c != '\"' && c != '\\') | escape)
-  private val unquotedChar: Parser[_] = P(CharsWhile(c => !c.isWhitespace && !"$\"{}[]:=,+#`^?!@*&\\".contains(c)))
-  private val rawStringChar: Parser[_] = P(CharsWhile(_ != '\"').~/ | "\"" ~ (CharsWhile(_ != '\"').~/ | "\"" ~ CharsWhile(_ != '\"').~/))
+  private val escapeChar: Parser[_] = P {
+    "\\" ~ (CharIn(strings = "\"\\\\/bfnrt") | unicode)
+  }
+  private val quotedChar: Parser[_] = P {
+    CharsWhile(c => c != '\"' && c != '\\') | escapeChar
+  }
+  private val unquotedChar: Parser[_] = P {
+    CharsWhile(c => !c.isWhitespace && !"$\"{}[]:=,+#`^?!@*&\\".contains(c))
+  }
+  private val rawStringChar: Parser[_] = P {
+    CharsWhile(_ != '\"').~/ | "\"" ~ (CharsWhile(_ != '\"').~/ | "\"" ~ CharsWhile(_ != '\"').~/)
+  }
 
-  private val nullPointerValue: Parser[NullPointer] = P("null").!.map(NullPointer)
-  private val number: Parser[Number] = P("-".? ~ numeric ~ P("." ~ digits).? ~ exponent.?).!.map(Number)
-  private val boolean: Parser[Boolean] = P("true" | "false" | "yes" | "no" | "on" | "off").!.map(Boolean)
-  private val timeUnit: Parser[TimeUnit] = P(numeric ~ spaces.rep ~ (timeUnitSuffix | timeUnitSuffixAbbr)).!.map(TimeUnit)
+  private val nullPointerValue: Parser[NullPointer] = P {
+    "null".!.map(NullPointer)
+  }
+  private val boolean: Parser[Boolean] = P {
+    ("true" | "false" | "yes" | "no" | "on" | "off").!.map(Boolean)
+  }
+  private val number: Parser[Number] = P {
+    ("-".? ~ numeric ~ P("." ~ digits).? ~ exponent.?).!.map(Number)
+  }
+  private val timeUnit: Parser[TimeUnit] = P {
+    (numeric ~ spaces.rep ~ (timeUnitSuffix | timeUnitSuffixAbbr)).!.map(TimeUnit)
+  }
 
-  private val quotedString: Parser[QuotedString] = P("\"" ~/ quotedChar.rep ~ "\"").!.map(QuotedString)
-  private val unquotedString: Parser[UnquotedString] = P(unquotedChar.rep(min = 1)).!.map(UnquotedString)
-  private val rawString: Parser[MultilineString] = P("\"\"\"" ~/ rawStringChar.rep ~ "\"".rep(min = 3)).!.map(MultilineString)
+  private val unquotedString: Parser[UnquotedString] = P {
+    unquotedChar.rep(min = 1).!.map(UnquotedString)
+  }
+  private val quotedString: Parser[QuotedString] = P {
+    ("\"" ~/ quotedChar.rep ~ "\"").!.map(QuotedString)
+  }
+  private val rawString: Parser[MultilineString] = P {
+    ("\"\"\"" ~/ rawStringChar.rep ~ "\"".rep(min = 3)).!.map(MultilineString)
+  }
 
-  private val stringValue: Parser[Element] = P(rawString | quotedString | spaces.!.map(Space) | unquotedString)
-  private val stringLikeValue: Parser[Element] = P(nullPointerValue | boolean | timeUnit | number | stringValue)
+  private val stringOnlyValue: Parser[Element] = P {
+    rawString | quotedString | spaces.!.map(Space) | unquotedString
+  }
+  private val stringLikeValue: Parser[Element] = P {
+    nullPointerValue | boolean | timeUnit | number | stringOnlyValue
+  }
 
-  private val fieldPathElement: Parser[String] = P(quotedString | unquotedString).map(_.value)
-  private val fieldPathPart: Parser[FieldPathPart] = P(fieldPathElement).map(s => FieldPathPart(s.mkString))
-  private val fieldPathExpression: Parser[FieldPath] = P(fieldPathPart.rep(min = 1, sep = P("."))).map(FieldPath)
+  private val fieldPathElement: Parser[String] = P {
+    (quotedString | unquotedString).map(_.value)
+  }
+  private val fieldPathPart: Parser[PathExpressionPart] = P {
+    fieldPathElement.map(s => PathExpressionPart(s.mkString))
+  }
+  private val fieldPathExpression: Parser[PathExpression] = P {
+    fieldPathPart.rep(min = 1, sep = P(".")).map(PathExpression)
+  }
 
-  private val substitutionWeak: Parser[SubstitutionWeak] = P("${?" ~ fieldPathExpression ~ "}").map(SubstitutionWeak)
-  private val substitutionStrong: Parser[SubstitutionStrong] = P("${" ~ fieldPathExpression ~ "}").map(SubstitutionStrong)
+  private val substitutionOptional: Parser[SubstitutionWeak] = P {
+    ("${?" ~ fieldPathExpression ~ "}").map(SubstitutionWeak)
+  }
+  private val substitution: Parser[SubstitutionStrong] = P {
+    ("${" ~ fieldPathExpression ~ "}").map(SubstitutionStrong)
+  }
 
-  private val substitution: Parser[Element] = P(substitutionWeak | substitutionStrong)
-  private val stringRepeatition: Parser[Seq[Element]] = P(substitution | stringValue).rep
-  private val listRepeatition: Parser[Seq[Element]] = P(substitution | listValue).rep(sep = spaces.?)
-  private val objectRepeatition: Parser[Seq[Element]] = P(substitution | objectValue).rep(sep = spaces.?)
+  private val stringRepeatition: Parser[Seq[(Int, Element)]] = P {
+    (Index ~ (substitutionOptional | substitution | stringOnlyValue)).rep
+  }
+  private val valueRepeatition: Parser[Seq[(Int, Element)]] = P {
+    (spaces.? ~ values.map(_.children)).map(_._2).?.map(_.getOrElse(Seq.empty))
+  }
+  private val listRepeatition: Parser[Seq[(Int, Element)]] = P {
+    (spaces.? ~ (Index ~ (substitutionOptional | substitution | listValue))).map(_._2).rep
+  }
+  private val objectRepeatition: Parser[Seq[(Int, Element)]] = P {
+    (spaces.? ~ (Index ~ (substitutionOptional | substitution | objectValue))).map(_._2).rep
+  }
 
-  private val fieldAppendValue: Parser[Concatenation] = P(spacesCrossLines ~ "+=" ~/ spacesCrossLines ~ values).map(t => Concatenation(t._3))
-  private val fieldObjectValue: Parser[Concatenation] = P(spacesCrossLines ~ objectValue ~ objectRepeatition).map(t => Concatenation(t._2 +: t._3))
-  private val fieldValue: Parser[Concatenation] = P(spacesCrossLines ~ CharIn(strings = ":=") ~/ spacesCrossLines ~ values).map(t => Concatenation(t._3))
+  private val listValues: Parser[Concatenation] = P {
+    (Index ~ listValue ~ listRepeatition ~ Index).map(Concatenation.tupled)
+  }
+  private val objectValues: Parser[Concatenation] = P {
+    (Index ~ objectValue ~ objectRepeatition ~ Index).map(Concatenation.tupled)
+  }
+  private val stringValues: Parser[Concatenation] = P {
+    (Index ~ stringLikeValue ~ stringRepeatition ~ Index).map(Concatenation.tupled)
+  }
+  private val substitutions: Parser[Concatenation] = P {
+    (Index ~ (substitutionOptional | substitution) ~ valueRepeatition ~ Index).map(Concatenation.tupled)
+  }
 
-  private val stringValues: Parser[Seq[Element]] = P(stringLikeValue ~ stringRepeatition).map(t => t._1 +: t._2)
-  private val listValues: Parser[Seq[Element]] = P(listValue ~ spaces.? ~ listRepeatition).map(t => t._1 +: t._3)
-  private val objectValues: Parser[Seq[Element]] = P(objectValue ~ spaces.? ~ objectRepeatition).map(t => t._1 +: t._3)
-  private val substitutions: Parser[Seq[Element]] = P(substitution ~ spaces.? ~ values.?).map(t => t._1 +: t._3.getOrElse(Seq.empty))
+  private val fieldObjectValue: Parser[(Int, Concatenation)] = P {
+    (spacesCrossLines ~ Index ~ objectValues).map(t => (t._2, t._3))
+  }
+  private val fieldAppendValue: Parser[(Int, Concatenation)] = P {
+    (spacesCrossLines ~ "+=" ~/ spacesCrossLines ~ Index ~ values).map(t => (t._3, t._4))
+  }
+  private val fieldValue: Parser[(Int, Concatenation)] = P {
+    (spacesCrossLines ~ CharIn(strings = ":=") ~/ spacesCrossLines ~ Index ~ values).map(t => (t._3, t._4))
+  }
+  private val field: Parser[ObjectElement] = P {
+    (Index ~ fieldPathExpression ~/ (fieldValue | fieldAppendValue | fieldObjectValue)).map(ObjectElement.tupled)
+  }
 
-  private val field: Parser[(FieldPath, Concatenation)] = P(fieldPathExpression ~/ (fieldValue | fieldAppendValue | fieldObjectValue))
-  private val fields: Parser[Seq[(FieldPath, Concatenation)]] = P(spacesCrossLines ~ field.rep(sep = valueSeparator) ~ spacesCrossLines).map(_._2)
-  private val elements: Parser[Seq[Concatenation]] = P(spacesCrossLines ~ values.map(Concatenation).rep(sep = valueSeparator) ~ spacesCrossLines).map(_._2)
+  private val fields: Parser[IndexedSeq[ObjectElement]] = P {
+    (spacesCrossLines ~ field.rep(sep = valueSeparator) ~ spacesCrossLines).map(_._2.toIndexedSeq)
+  }
+  private val elements: Parser[IndexedSeq[ListElement]] = P {
+    (spacesCrossLines ~ (Index ~ values).map(ListElement.tupled).rep(sep = valueSeparator) ~ spacesCrossLines).map(_._2.toIndexedSeq)
+  }
 
-  private def rootValue: Parser[Element] = P(objectValue | listValue | fields.map(Object))
+  private def values: Parser[Concatenation] = P {
+    substitutions | stringValues | listValues | objectValues
+  }
 
-  private def listValue: Parser[List] = P("[" ~/ elements ~ "]" ~ spaces.?).map(t => List(t._1))
+  private def rootObjectValue: Parser[Object] = P {
+    (Index ~ fields ~ Index).map(Object.tupled)
+  }
 
-  private def objectValue: Parser[Object] = P("{" ~/ fields ~ "}" ~ spaces.?).map(t => Object(t._1))
+  private def listValue: Parser[List] = P {
+    (Index ~ "[" ~/ elements ~ "]" ~ Index).map(List.tupled)
+  }
 
-  private def values: Parser[Seq[Element]] = P(substitutions | stringValues | listValues | objectValues)
+  private def objectValue: Parser[Object] = P {
+    (Index ~ "{" ~/ fields ~ "}" ~ Index).map(Object.tupled)
+  }
 
-  def root: Parser[Element] = P(Start ~ spacesCrossLines ~ rootValue ~ spacesCrossLines ~ End).map(t => t._2)
+  def root: Parser[Element] = P {
+    (Start ~ spacesCrossLines ~ (objectValue | listValue | rootObjectValue) ~ spacesCrossLines ~ End).map(_._2)
+  }
 }
